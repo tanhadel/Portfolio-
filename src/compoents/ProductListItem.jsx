59be3349db5904d5de5b596a89e84278999fdcd7
Taha the one <taha.taheri@yh.nackademin.se>
2024-01-11 11:59:42 +0100

    last update

---
+++ src/compoents/ProductListItem.jsx
@@ -0,0 +1,17 @@
+
+import styled from 'styled-components'
+const Box = styled.div `
+width: 100px;
+height: 100px;
+background-color: red;
+
+`
+
+function ProductListItem(){
+    return (
+        <Box>
+            Product List Item
+        </Box>
+    )
+}
+export default ProductListItem;
